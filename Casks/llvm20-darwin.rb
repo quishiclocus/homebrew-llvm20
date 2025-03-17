253c087a4fad2e509cc7e48f8e817d50b0609736
Chuck Stearns <chuck.stearns@gmail.com>
2025-03-17 12:32:07 -0500

    Fixed version string

--- Casks/llvm20-darwin.rb
+++ Casks/llvm20-darwin.rb
@@ -1,5 +1,5 @@
 cask 'llvm20-darwin' do
-  version 20.1
+  version "20.1.0"
   sha256 :no_check
 
   url "https://github.com/llvm/llvm-project/releases/download/llvmorg-20.1.0/LLVM-20.1.0-macOS-ARM64.tar.xz"
