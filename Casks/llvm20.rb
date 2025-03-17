74ded5da5f467c16ac2df7cd959d20858e85a55d
Chuck Stearns <chuck.stearns@gmail.com>
2025-03-17 12:26:23 -0500

    Initial commit

---
+++ Casks/llvm20.rb
@@ -0,0 +1,8 @@
+cask 'neovim-nightly-m1' do
+  version :20.1.0
+  sha256 :no_check
+
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-20.1.0/LLVM-20.1.0-macOS-ARM64.tar.xz"
+  name 'LLVM 20.1.0 (aarch64-darwin)'
+  homepage 'https://llvm.org/'
+end
